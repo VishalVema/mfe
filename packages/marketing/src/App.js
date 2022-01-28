ca9a9cf645f28f94dae7f3379c0bef1560e88fe4
Rocky Verma <vishalvermadeveloper@gmail.com>
2022-01-28 22:20:00 +0530

    trigger build

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,7 +6,7 @@ import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 export default () => {
-    return (<div>
+    return (<div> 
         <StylesProvider>
             <BrowserRouter>
                 <Route exact path="/pricing" component={Pricing} />
