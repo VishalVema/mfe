fe486c61348efa8f7178a63f38ce9751605d44d1
Rocky Verma <vishalvermadeveloper@gmail.com>
2022-01-28 23:14:24 +0530

    add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,16 @@
-import React, { useEffect, useRef } from  'react';
+import React, { useEffect, useRef } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/header';
 
-export default () =>  {
-    
-    return <div>
-            <h1>Hi there!!!</h1>
-            <hr/>
-            <MarketingApp/>
-        </div>
-} 
+export default () => {
+
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    )
+}
